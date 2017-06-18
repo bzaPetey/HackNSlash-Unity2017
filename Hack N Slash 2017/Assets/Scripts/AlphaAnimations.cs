171771b71f55573fe4ef39e2ff7ab2cfaf7c5e64
Unknown <peter.laliberte@burgzergarcade.net>
2017-06-17 21:21:13 -0400

    Add the Beta chatacter from Mixamo and the spell caster pro pack of animations.
    Updated project files from the second day of streaming,

---
+++ Hack N Slash 2017/Assets/Scripts/AlphaAnimations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+public class AlphaAnimations : MonoBehaviour {
+    Animator anim;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        anim.SetFloat("speed", Input.GetAxis("Vertical"));
+    }
+}
